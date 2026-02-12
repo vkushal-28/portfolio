2d7d364de83e76c619d0b5b0a0049993a8d163bf
vkushal-28 <vkushal28.ca@gmail.com>
2026-02-12 18:06:34 -0500

    updated for github and vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/portfolio/",
+  base: mode === "github" ? "/portfolio/" : "/",
   assetsInclude: "**/*.docx",
-});
+}));
